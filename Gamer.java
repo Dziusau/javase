353a0c81eb8da7ef6fc9f493e0e6ead70699e263
Dziusau <43704303+Dziusau@users.noreply.github.com>
2018-10-27 18:24:33 +0300

    Add files via upload

---
+++ Gamer.java
@@ -0,0 +1,8 @@
+public class Gamer {
+    String nickname = "Trololo";
+    int age = 17;
+    int cash = 35;
+    void buy (Game) {
+
+    }
+}
---
+++ Steam.java
@@ -0,0 +1,66 @@
+public class Steam {
+    int totalCash = 0;
+    void Game() {
+        Game[] play = new Game[10];
+        int i = 0;
+        play[i] = new Game();
+            play[i].name = "Dota 2";
+            play[i].cost = 5;
+            play[i].ageLimit = 8;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "CS GO";
+            play[i].cost = 10;
+            play[i].ageLimit = 12;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "Breathedge";
+            play[i].cost = 3;
+            play[i].ageLimit = 13;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "";
+            play[i].cost = 10;
+            play[i].ageLimit = 8;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "Dota 2";
+            play[i].cost = 10;
+            play[i].ageLimit = 8;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "Dota 2";
+            play[i].cost = 10;
+            play[i].ageLimit = 8;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "Dota 2";
+            play[i].cost = 10;
+            play[i].ageLimit = 8;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "Dota 2";
+            play[i].cost = 10;
+            play[i].ageLimit = 8;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "Dota 2";
+            play[i].cost = 10;
+            play[i].ageLimit = 8;
+            i++;
+
+        play[i] = new Game();
+            play[i].name = "Dota 2";
+            play[i].cost = 10;
+            play[i].ageLimit = 8;
+
+    }
+}
