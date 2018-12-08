7a769c001850522c705a872996933c4f4fe967b8
Dziusau <43704303+Dziusau@users.noreply.github.com>
2018-12-08 14:39:57 +0300

    Add files via upload

--- CalculatorController.java
+++ CalculatorController.java
@@ -1,9 +1,11 @@
-package calculator.controller;
+package controller;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import java.awt.*;
 
 public class CalculatorController {
     @FXML
@@ -16,54 +18,67 @@ public class CalculatorController {
         display.textProperty().bind(calculatorDisplay);
     }
 
+    @FXML
+    private TextField currentState;
+
     public void button0Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 0 is clicked");
-        calculatorDisplay.set("0");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "0");
     }
 
     public void button1Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 1 is clicked");
-        calculatorDisplay.set("1");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "1");
     }
 
     public void button2Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 2 is clicked");
-        calculatorDisplay.set("2");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "2");
     }
 
     public void button3Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 3 is clicked");
-        calculatorDisplay.set("3");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "3");
     }
 
     public void button4Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 4 is clicked");
-        calculatorDisplay.set("4");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "4");
     }
 
     public void button5Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 5 is clicked");
-        calculatorDisplay.set("5");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "5");
     }
 
     public void button6Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 6 is clicked");
-        calculatorDisplay.set("6");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "6");
     }
 
     public void button7Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 7 is clicked");
-        calculatorDisplay.set("7");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "7");
     }
 
     public void button8Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 8 is clicked");
-        calculatorDisplay.set("8");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "8");
     }
 
     public void button9Clicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button 9 is clicked");
-        calculatorDisplay.set("9");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "9");
     }
 
     public void buttonCleanClicked (javafx.event.ActionEvent actionEvent){
@@ -71,47 +86,40 @@ public class CalculatorController {
         calculatorDisplay.set("");
     }
 
-    public void buttonDelClicked (javafx.event.ActionEvent actionEvent){
-        System.out.println("button Delete is clicked");
-        calculatorDisplay.set("");
-    }
-
     public void buttonSumClicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button + is clicked");
-        calculatorDisplay.set("+");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "+");
     }
 
     public void buttonDifClicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button - is clicked");
-        calculatorDisplay.set("-");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "-");
     }
 
     public void buttonOpClicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button * is clicked");
-        calculatorDisplay.set("*");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "*");
     }
 
     public void buttonDivClicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button / is clicked");
-        calculatorDisplay.set("/");
-    }
-
-    public void buttonBktClicked (javafx.event.ActionEvent actionEvent){
-        System.out.println("button () is clicked");
-        calculatorDisplay.set("()");
-    }
-
-    public void buttonPctClicked (ActionEvent actionEvent){
-        System.out.println("button % is clicked");
-        calculatorDisplay.set("%");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + "/");
     }
 
     public void buttonPtClicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button . is clicked");
-        calculatorDisplay.set(".");
+        currentState.getChars(TextField);
+        calculatorDisplay.set(currentState + ".");
     }
 
     public void buttonParClicked (javafx.event.ActionEvent actionEvent){
         System.out.println("button = is clicked");
+
     }
+
+    public void
 }
